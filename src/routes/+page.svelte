d33797db6508f4ae6344721ea4cd433a3d1ed084
blazzjosh <blazzjosh@live.com>
2023-05-31 15:09:24 +0100

    Removed Marquee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,76 +1,50 @@
 <script>
-	import Card from "$lib/components/Card.svelte";
-	import Icon from "$lib/components/Icon.svelte";
-	import NewsLetterForm from "$lib/components/NewsLetterForm.svelte";
+  import Card from "$lib/components/Card.svelte"
+  import Icon from "$lib/components/Icon.svelte"
+  import NewsLetterForm from "$lib/components/NewsLetterForm.svelte"
   import Photos from "$lib/components/Photos.svelte"
-	import Resume from "$lib/components/Resume.svelte";
-	import {articles} from './articles/data'
+  import Resume from "$lib/components/Resume.svelte"
+  import { articles } from "./articles/data"
 </script>
 
 <svelte:head>
-	<title>
-		Josh Horsfall - Efficient Full-Stack Leader: Let's Build Something Amazing!
-	</title>
-	<meta
-		name="description"
-		content="I’m Josh, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
-	/>
+  <title>Josh Horsfall - Efficient Full-Stack Leader: Let's Build Something Amazing!</title>
+  <meta name="description" content="I’m Josh, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms." />
 </svelte:head>
 
 <section class="mx-auto max-w-2xl lg:max-w-5xl p-4">
-	<div class="max-w-2xl">
-		<h1
-			class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
-		>
-		Full-stack software engineer, and problem solver.
-		</h1>
-		<p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-			I'm Josh Horsfall, an efficient full-stack software engineer. With over 3 years of experience, I specialize in HTML5, CSS3, JavaScript, Node.JS, React, and databases. Let's collaborate to build something amazing!
-		</p>
-		<div class="mt-6 flex gap-6">
-			<a class="" target="_blank" href="https://twitter.com/cradlydev">
-				<Icon
-					name="twitter"
-					class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300"
-				/>
-			</a>
-			<a class="" target="_blank" href="https://www.instagram.com/josh__hh/">
-				<Icon
-					name="instagram"
-					class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300"
-				/>
-			</a>
-			<a class="" target="_blank" href="https://github.com/blazzjosh">
-				<Icon
-					name="github"
-					class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300"
-				/>
-			</a>
-			<a class="" target="_blank" href="https://www.linkedin.com/in/cradlyjosh/">
-				<Icon
-					name="linkedin"
-					class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300"
-				/>
-			</a>
-		</div>
-	</div>
+  <div class="max-w-2xl">
+    <h1 class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">Full-stack software engineer, and problem solver.</h1>
+    <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">I'm Josh Horsfall, an efficient full-stack software engineer. With over 3 years of experience, I specialize in HTML5, CSS3, JavaScript, Node.JS, React, and databases. Let's collaborate to build something amazing!</p>
+    <div class="mt-6 flex gap-6">
+      <a class="" target="_blank" href="https://twitter.com/cradlydev">
+        <Icon name="twitter" class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300" />
+      </a>
+      <a class="" target="_blank" href="https://www.instagram.com/josh__hh/">
+        <Icon name="instagram" class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300" />
+      </a>
+      <a class="" target="_blank" href="https://github.com/blazzjosh">
+        <Icon name="github" class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300" />
+      </a>
+      <a class="" target="_blank" href="https://www.linkedin.com/in/cradlyjosh/">
+        <Icon name="linkedin" class="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:hover:fill-zinc-300" />
+      </a>
+    </div>
+  </div>
 </section>
 
-
-<Photos/>
-
-
+<Photos />
 
 <section class="mx-auto max-w-2xl lg:max-w-5xl mt-24 md:mt-28 p-4">
-    <div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-      <div class="flex flex-col gap-16">
-		{#each articles as article}
-        	<Card {article}/>
-		{/each}
-      </div>
-      <div class="space-y-10 lg:pl-16 xl:pl-24">
-        <NewsLetterForm />
-        <Resume />
-      </div>
+  <div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+    <div class="flex flex-col gap-16">
+      {#each articles as article}
+        <Card {article} />
+      {/each}
     </div>
-  </section>
+    <div class="space-y-10 lg:pl-16 xl:pl-24">
+      <NewsLetterForm />
+      <Resume />
+    </div>
+  </div>
+</section>
